5858635f7be5f69dc925b85fe5cdae6e9dae640a
Alexey Kisel <kisel.alexey@gmail.com>
2019-07-29 00:27:33 +0300

    support vue-routin

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,9 @@ var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
 app.use(serveStatic(__dirname))
+app.get('/*', function(request, request){
+  request.sendFile(__dirname+'/index.html');
+});
 var port = process.env.PORT || 8080
 app.listen(port)
 console.log('server started ' + port)
